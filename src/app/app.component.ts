47f7b772eab3e991cd59bb50193e70a4e90264f3
VaaaZeee <t.tivadar1999@gmail.com>
2023-06-28 18:12:01 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'dnd-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+})
+export class AppComponent {
+  public environmentInjector = inject(EnvironmentInjector);
+
+  constructor() {}
+}
