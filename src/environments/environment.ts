234b5b3c7afc565f4925136dc38ffd38604f0ed8
VaaaZeee <t.tivadar1999@gmail.com>
2024-04-17 22:14:07 +0200

    update firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
+    projectId: 'dnd-session-manager',
+    appId: '1:1032114902120:web:8d6f54ba4144c3b90fd7dd',
+    storageBucket: 'dnd-session-manager.appspot.com',
     apiKey: 'AIzaSyDxGlaLphM731BYuu-gAeWbbSsnpQQhjHo',
     authDomain: 'dnd-session-manager.firebaseapp.com',
-    projectId: 'dnd-session-manager',
-    storageBucket: 'dnd-session-manager.appspot.com',
     messagingSenderId: '1032114902120',
-    appId: '1:1032114902120:web:8d6f54ba4144c3b90fd7dd',
     measurementId: 'G-EJZ8TNXYMY',
   },
 };
